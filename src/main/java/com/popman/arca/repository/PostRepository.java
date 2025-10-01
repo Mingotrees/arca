4bef2c9684f1d1e5274750819f7a1d5f54cd0b41
Mingotrees <jhanell.mingo@gmail.com>
2025-10-01 15:54:44 +0800

    feat: merge initial

--- src/main/java/com/popman/arca/repository/PostRepository.java
+++ src/main/java/com/popman/arca/repository/PostRepository.java
@@ -1,7 +1,8 @@
 package com.popman.arca.repository;
 
+import com.popman.arca.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, String> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
