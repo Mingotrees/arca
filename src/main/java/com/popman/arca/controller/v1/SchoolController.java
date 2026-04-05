65a41e9baaa8f62ebdf456861a6e1866e2399b31
Miggy Carumba <miggycarumba912@gmail.com>
2026-04-05 20:45:56 +0800

    refactor: removed "/add"

--- src/main/java/com/popman/arca/controller/v1/SchoolController.java
+++ src/main/java/com/popman/arca/controller/v1/SchoolController.java
@@ -12,64 +12,60 @@ import java.util.List;
 @RequestMapping("api/v1/schools")
 public class SchoolController {
 
-    private final SchoolService schoolService;
+  private final SchoolService schoolService;
 
-    public SchoolController(SchoolService schoolService){
-        this.schoolService = schoolService;
+  public SchoolController(SchoolService schoolService) {
+    this.schoolService = schoolService;
+  }
+
+  @PostMapping
+  public ResponseEntity<?> addSchool(@RequestBody SchoolRequest request) {
+    try {
+      SchoolResponse created = schoolService.addSchoolV1(request);
+      return ResponseEntity.ok(created);
+    } catch (RuntimeException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body("An unexpected error occured");
     }
 
+  }
 
-    @PostMapping("/add")
-    public ResponseEntity<?> addSchool(@RequestBody SchoolRequest request){
-        try{
-            SchoolResponse created = schoolService.addSchoolV1(request);
-            return ResponseEntity.ok(created);
-        }catch (RuntimeException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }catch (Exception e){
-            return ResponseEntity.internalServerError().body("An unexpected error occured");
-        }
-
-
+  @GetMapping
+  public ResponseEntity<?> getAllSchools() {
+    try {
+      List<SchoolResponse> schools = schoolService.getAllSchoolV1();
+      if (schools.isEmpty()) {
+        return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.ok(schools);
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body("Error fetching schools");
     }
+  }
 
-    @GetMapping
-    public ResponseEntity<?> getAllSchools(){
-        try{
-            List<SchoolResponse> schools = schoolService.getAllSchoolV1();
-            if(schools.isEmpty()){
-                return ResponseEntity.notFound().build();
-            }
-            return ResponseEntity.ok(schools);
-        }catch (Exception e){
-            return ResponseEntity.internalServerError().body("Error fetching schools");
-        }
+  @PutMapping("/edit/{id}")
+  public ResponseEntity<?> editSchool(@PathVariable Long id, @RequestBody SchoolRequest request) {
+
+    try {
+      SchoolResponse updateSchool = schoolService.editSchoolV1(id, request);
+      return ResponseEntity.ok(updateSchool);
+    } catch (RuntimeException e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
     }
+  }
 
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<?> editSchool(@PathVariable Long id, @RequestBody SchoolRequest request){
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<?> deleteSchool(@PathVariable Long id) {
+    try {
 
-        try {
-            SchoolResponse updateSchool = schoolService.editSchoolV1(id, request);
-            return ResponseEntity.ok(updateSchool);
-        }catch (RuntimeException e){
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+      schoolService.deleteSchoolV1(id);
+      return ResponseEntity.ok("School deleted successfully");
+    } catch (RuntimeException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body("An error has occurred");
     }
-
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteSchool(@PathVariable Long id){
-        try {
-
-            schoolService.deleteSchoolV1(id);
-            return ResponseEntity.ok("School deleted successfully");
-        }catch (RuntimeException e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }catch (Exception e){
-            return ResponseEntity.internalServerError().body("An error has occurred");
-        }
-    }
-
+  }
 
 }
